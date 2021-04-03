36fe39b60fa0332e046a96a021b210ab542c8472
SD-DS420 <80000995+SD-DS420@users.noreply.github.com>
2021-04-03 11:00:52 -0400

    Update UpdatedSQLDatabase.sql

--- UpdatedSQLDatabase.sql
+++ UpdatedSQLDatabase.sql
@@ -21,72 +21,30 @@ GRANT ALL PRIVILEGES ON ClassLLuser.* TO 'ClassLLuser';
 CREATE TABLE LoggedInandLoggedOut
 (
     ClassID INTEGER PRIMARY KEY NOT NULL,
-    LastName INTEGER NOT NULL,
-    TheTime TIME NOT NULL,
-    TheDate DATE NOT NULL,
-    LoginStatus INTEGER
+    LastNameID INTEGER NOT NULL,
+    TimeID TIME NOT NULL,
+    DateID DATE NOT NULL,
+    StatusID INTEGER
 );
 
 -- LastNames Table
-CREATE TABLE LastNames
-(
-    Lastnames INTEGER PRIMARY KEY NOT NULL,
-    AssociatedClassID INTEGER NOT NULL
-);
-
--- Classes Table
-CREATE TABLE Classes
-(
-    ClassID INTEGER PRIMARY KEY NOT NULL
-);
-
--- ClassLogin Database
-
--- Drop database if exists
-DROP DATABASE IF EXISTS ClassLoginLogout;
-
--- Create database
-CREATE DATABASE ClassLoginLogout;
-USE ClassLoginLogout;
-
--- Drop user if exists
-DROP USER IF EXISTS 'ClassLLUser'@'localhost';
-
--- Create user
-CREATE USER 'ClassLLUser' IDENTIFIED BY 'Aakjdsg4R0!';
-
--- Gives user permissions
-GRANT ALL PRIVILEGES ON ClassLLuser.* TO 'ClassLLuser';
-
--- LoggedInandLoggedOut Table
-CREATE TABLE LoggedInandLoggedOut
-(
-    ClassID INTEGER PRIMARY KEY NOT NULL,
-    LastName INTEGER NOT NULL,
-    AssociatedClassID INTEGER NOT NULL,
-    TheTime TIME NOT NULL,
-    TheDate DATE NOT NULL,
-    LoginStatus INTEGER
-);
-
--- UserData Table
 CREATE TABLE UserData
 (
-    Lastnames INTEGER PRIMARY KEY NOT NULL,
+    LastnameID INTEGER PRIMARY KEY NOT NULL,
     AssociatedClassID INTEGER NOT NULL
 );
 
 -- Classes Table
-CREATE TABLE Classes
+CREATE TABLE ClasseNumbers
 (
     ClassID INTEGER PRIMARY KEY NOT NULL
 );
 
 SELECT *
-FROM LoggedInandLoggedOut, Classes, UserData
+FROM LoggedInandLoggedOut, ClassNumbers, UserData
 INNER JOIN Classes
-ON LoggedInandLoggedOut.ClassID = Classes.ClassID
+ON LoggedInandLoggedOut.ClassID = ClassNumbers.ClassID
 INNER JOIN UserData
-ON LoggedInandLoggedOut.LastName = UserData.Lastname
+ON LoggedInandLoggedOut.LastNameID = UserData.LastnameID
 INNER JOIN UserData
 ON LoggedInandLoggedOut.AssociatedClassID = UserData.AssociatedClassID
