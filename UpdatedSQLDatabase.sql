4e1477a9fd920d4b4bf6f0dc9f07440322de92e7
Flamelier <34852960+flamelier@users.noreply.github.com>
2021-04-09 21:14:18 -0400

    Update UpdatedSQLDatabase.sql

--- UpdatedSQLDatabase.sql
+++ UpdatedSQLDatabase.sql
@@ -3,28 +3,36 @@
 
 -- Drop database if exists
 DROP DATABASE IF EXISTS ClassLoginLogout;
+GO
 
 -- Create database
 CREATE DATABASE ClassLoginLogout;
 USE ClassLoginLogout;
+GO
 
 -- Drop user if exists
 DROP USER IF EXISTS 'ClientUser'@'localhost';
+GO
 
 -- Create user
 CREATE USER 'ClientUser' IDENTIFIED BY 'Aakjdsg4R0!';
+GO
 
 -- Gives user permissions
 GRANT ALL PRIVILEGES ON ClassLoginLogout.* TO 'ClientUser';
+GO
 
 -- Drop user if exists
 DROP USER IF EXISTS 'PanelUser'@'localhost';
+GO
 
 -- Create user
 CREATE USER 'PanelUser' IDENTIFIED BY 'Bblketh5S1@';
+GO
 
 -- Gives user permissions
-GRANT ALL PRIVILEGES ON ClassLoginLogout.Logs TO 'PanelUser';
+GRANT INSERT ON ClassLoginLogout.Logs TO 'PanelUser';
+GO
 
 -- LoggedInandLoggedOut Table
 CREATE TABLE Logs
@@ -35,6 +43,7 @@ CREATE TABLE Logs
     DateID DATE NOT NULL,
     LoginStatusID INTEGER
 );
+GO
 
 -- LastNames Table
 CREATE TABLE UserData
@@ -42,12 +51,14 @@ CREATE TABLE UserData
     LastnameID INTEGER PRIMARY KEY NOT NULL,
     AssociatedClassID INTEGER NOT NULL
 );
+GO
 
 -- Classes Table
 CREATE TABLE ClassNumbers
 (
     ClassID INTEGER PRIMARY KEY NOT NULL
 );
+GO
 
 SELECT *
 FROM Logs, ClassNumbers, UserData
@@ -57,3 +68,4 @@ INNER JOIN UserData
 ON Logs.LastNameID = UserData.LastnameID
 INNER JOIN UserData
 ON Logs.AssociatedClassID = UserData.AssociatedClassID
+GO
